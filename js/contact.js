a650dc212c94ad8002e843c75b904cdf9fe92c11
HJska27 <kallvey.thong23@student.cart.org>
2024-05-14 09:39:06 -0700

    s

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,35 @@
-function showItems(entries){
-    
+let listitems = [...document.querySelectorAll('li')];
+
+let options = {
+    rootMargin: '-10%',
+    threshold: 0.0
 }
+
+let observer = new IntersectionObserver(showItem, options);
+
+function showItem(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            let letters = [...entry.target.querySelectorAll('span')];
+            letters.forEach((letter, idx) => {
+                setTimeout(()=>{
+                    letter.classList.add('active');
+                }, idx * 10)
+            })
+            entry.target.children[0].classList.add('active');
+        }
+    })
+}
+
+function takeMeToLinkedIn() {
+    window.open('https://www.linkedin.com/in/kallvey-thong-581375308/', '_blank')
+}
+
+listitems.forEach(item =>{
+    item.onclick = takeMeToLinkedIn;
+    let newString = '';
+    let itemText = item.children[0].innerText.split('');
+    itemText.map(letter => (newString += letter == ' ' ? `<span class='gap'></span>` : `<span>${letter}</span>`))
+    item.innerHTML = newString;
+    observer.observe(item);
+})
